a175115e727687c743ee6f13baf911a9cfe53b8c
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2024-02-22 20:09:17 +0700

    Add project files.

---
+++ Migrations/20240222125630_createProfile.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPTJOB.Migrations
+{
+    /// <inheritdoc />
+    public partial class createProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfileId",
+                table: "Jobs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Profile",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Introduction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthday = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_ProfileId",
+                table: "Jobs",
+                column: "ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_Profile_ProfileId",
+                table: "Jobs",
+                column: "ProfileId",
+                principalTable: "Profile",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_Profile_ProfileId",
+                table: "Jobs");
+
+            migrationBuilder.DropTable(
+                name: "Profile");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_ProfileId",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileId",
+                table: "Jobs");
+        }
+    }
+}
---
+++ Models/DBMyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FPTJOB.Models;
+
+namespace FPTJOB.Models
+{
+    public class DBMyContext : IdentityDbContext
+    {
+        public DBMyContext(DbContextOptions<DBMyContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<FPTJOB.Models.Profile> Profile { get; set; } = default!;
+    }
+}
