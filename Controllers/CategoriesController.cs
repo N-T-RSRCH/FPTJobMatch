282fa346c89198885f062dd917a1f7bb4e444f0b
Le Nguyen Huy Hoang <hoanglnhgcc210076@fpt.edu.vn>
2024-03-06 15:43:18 +0700

    Hoang commit

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -47,7 +47,7 @@ namespace FPTJOB.Controllers
         {
             return View();
         }
-
+        // Hoang
         // POST: Categories/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
