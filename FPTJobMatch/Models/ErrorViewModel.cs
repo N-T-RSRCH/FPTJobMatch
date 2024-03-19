066611ae2f0b3bfeb21ad0111595c19b1c552eba
N.T <150209071+N-T-RSRCH@users.noreply.github.com>
2024-03-19 22:02:18 +0700

    Delete FPTJobMatch directory

--- FPTJobMatch/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FPTJobMatch.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
