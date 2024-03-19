d0a50c4239ac71f2e73854696e3f4355914f0fab
N.T <150209071+N-T-RSRCH@users.noreply.github.com>
2024-03-19 21:34:19 +0700

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FPTJOB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
